295a2c0eb405aaa9d8b6e63820bc7fdbb1188608
Deva-CN <109334955+lenstark@users.noreply.github.com>
2022-12-30 17:10:20 +0530

    main file

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-// const port = 3000;
+
 const app = express();
 const userRouter = require('./app/routes/Routes.js')
 require("dotenv").config();
